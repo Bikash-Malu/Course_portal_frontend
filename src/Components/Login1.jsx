10b25ab1e3a069e123c4312294c41d1359e17aa4
startgit <bikashmalu1@gmail.com>
2023-11-04 22:31:07 +0530

    add

--- src/Components/Login1.jsx
+++ src/Components/Login1.jsx
@@ -2,13 +2,46 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import Swal from 'sweetalert2'
+import { login } from './user-service'
+import { dologin } from './auto'
 const Login1 = (props) => {
+  const navigate1=useNavigate();
   
-
-
+const[logindetail,setlogindetail]=useState({
+  email:'',
+  password:''
+})
+const handleChange=(event,field)=>{
+ let avalue=event.target.value;
+ setlogindetail({
+  ...logindetail,[field]:avalue
+ })
+}
+const formsubmit=(event)=>{
+  event.preventDefault();
+  console.log(logindetail);
+  if(logindetail.email.trim()==''||logindetail.password.trim()==''){
+    Swal.fire("fill all the feild");
+    return;
+  }
+  //submit the data to the server
+  login(logindetail).then((data)=>{
+    console.log("user login");
+    Swal.fire("login success");
+    console.log(data)
+    //save the to local storage
+    dologin(data,()=>{
+      console.log("save to local storage");
+    })
+  }).catch(error=>{
+    // console.log(error);
+    Swal.fire("something went wrong")
+  })
+}
   return (
     <div>
-        <form className='h-50 mt-2'>
+        <form className='h-50 mt-2' onSubmit={formsubmit}>
       <section className="h-50 gradient-custom mt-5"style={{height:"300px",background:"linear-gradient(to right,bisque,pink,bisque,#FDE5D4)"}}>
         <Navbar/>
       <title>{props.title}</title>
@@ -22,12 +55,12 @@ const Login1 = (props) => {
               <p style={{fontSize:'20px'}} className="text-white-50 mb-5">Please enter your login and password!</p>
              <br/> <label className="form-label">Email</label>
               <div className="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" className="form-control form-control-lg" name='email' placeholder='enter your email' />
+                <input type="email" id="typeEmailX" className="form-control form-control-lg" name='email' placeholder='enter your email' value={logindetail.email} onChange={(e)=>handleChange(e,'email')} />
               
               </div>
               <label className="form-label" >Password</label>
               <div className="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" className="form-control form-control-lg" name='password' placeholder='enter your password' />
+                <input type="password" id="typePasswordX" className="form-control form-control-lg" name='password' placeholder='enter your password' value={logindetail.password} onChange={(e)=>handleChange(e,'password')}/>
                 
               </div>
 
